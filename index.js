6b61847b3aab41ee0a70ac90ff3bf39b34d9de8a
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-10-08 19:53:05 -0300

    agregue las funciones que calculan descueno, recargo y envio

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ const mostrarRecargo = document.getElementById("recargo")
 const mostarDescuento = document.getElementById("descuento")
 const mostrarEnvio = document.getElementById("envio")
 const mostarTotal = document.getElementById("total")
-const subtotal = 55498
+const subtotalProductos = 55000
+const gastoDeEnvio = 50
 
 
 
+//funcion que calcula el 10% de recargo por pago con tarjeta
+const obtenerRecargo = (subtotalProductos) => {
+    const calculoRecargo = subtotalProductos * 0.1
+    return calculoRecargo
+}
+console.log (obtenerRecargo(subtotalProductos))
 
 
+//funcion que calcula el 10% de descuento
+const obtenerDescuento = (subtotalProductos) => {
+    const calculoDescuento = subtotalProductos - subtotalProductos * 0.1
+    return calculoDescuento
+}
+console.log (obtenerDescuento(subtotalProductos))
 
+//funcion que suma costo de envío de 50
+const obtenerEnvio = (subtotalProductos)=>{
+    const calculoEnvio = subtotalProductos + 50
+    return calculoEnvio
+}
+console.log (obtenerEnvio(subtotalProductos))
