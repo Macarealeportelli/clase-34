a822b86209a39abbf42c6bd4d8fd3be8d9bd03f4
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-10-08 18:12:21 -0300

    iniciando ejercicio del carrito en JS

---
+++ index.js
@@ -0,0 +1,16 @@
+const mostrarSubtotal = document.getElementById("subtotal")
+const pagaConTarjeta = document.getElementById("tarjeta")
+const tieneDescuento = document.getElementById("tiene-descuento")
+const tieneEnvio = document.getElementById("tiene-envio")
+const pagaConEfectivo = document.getElementById("efectivo")
+const mostrarRecargo = document.getElementById("recargo")
+const mostarDescuento = document.getElementById("descuento")
+const mostrarEnvio = document.getElementById("envio")
+const mostarTotal = document.getElementById("total")
+const subtotal = 55498
+
+
+
+
+
+
